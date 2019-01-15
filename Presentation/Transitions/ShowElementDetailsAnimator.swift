7adf853a3acb5492b256c316d3f5b4b8fb90b9be
Mateusz Matoszko <mateusz@matoszko.pl>
2019-01-15 20:55:28 +0100

    Add base animation classes

---
+++ Presentation/Transitions/ShowElementDetailsAnimator.swift
@@ -0,0 +1,9 @@
+//
+//  ShowElementDetailsAnimator.swift
+//  StoreTransitions
+//
+//  Created by Mateusz Matoszko on 15/01/2019.
+//  Copyright © 2019 Mateusz Matoszko. All rights reserved.
+//
+
+import Foundation
